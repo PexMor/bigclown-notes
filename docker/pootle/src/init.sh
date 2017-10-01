d006fe267dee0bbb3df2da48f43e886f4568bd6e
PexMor <p.moravek@xlinux.cz>
2017-10-01 23:49:05 +0200

    add few hints into init script

--- docker/pootle/src/init.sh
+++ docker/pootle/src/init.sh
@@ -10,3 +10,17 @@ pip install --process-dependency-links Pootle[mysql]
 
 pootle init --db mysql --db-name pootledb --db-user pootle --db-host mysql
 
+echo "overwrite the config with the config provided here"
+
+# cp ...conf trg/
+
+pootle initdb --no-projects
+
+pootle createsuperuser --username admin --no-input --email test@example.com
+pootle verify_user admin
+
+echo "please run 'pootle changepassword admin'"
+
+# pootle createsuperuser
+# pootle verify_user admin
+# pootle runserver --insecure
